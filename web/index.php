f3ab707ffe93b6e53b61bfbdbbfd54909f27abc8
tak <tak.tak@kyp.biglobe.ne.jp>
2016-09-09 15:44:50 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -141,7 +141,7 @@ if(  count(  $items  ) < 16 ){
                     	[
         		"title"=> $items[5]  ,
         		"image_url"=> "http://static.mealthy.me/uploads/menu/image/".  $items[8]  , 
-        		"subtitle"=>  $items[6]."kcal ".  $items[7]  ."円    ". $items[0]     ,
+        		"subtitle"=>  $items[6]." ".  $items[7]  ."円    ". $items[0]     ,
         		"buttons"=> [
         			[
         			"type"=> "web_url",
@@ -156,7 +156,7 @@ if(  count(  $items  ) < 16 ){
                     	[
         		"title"=> $items[9]  ,
         		"image_url"=> "http://static.mealthy.me/uploads/menu/image/".  $items[12]  , 
-        		"subtitle"=>  $items[10]."kcal ".  $items[11]  ."円    ". $items[0]     ,
+        		"subtitle"=>  $items[10]." ".  $items[11]  ."円    ". $items[0]     ,
         		"buttons"=> [
         			[
         			"type"=> "web_url",
@@ -169,7 +169,7 @@ if(  count(  $items  ) < 16 ){
                     	[
         		"title"=> $items[13]  ,
         		"image_url"=> "http://static.mealthy.me/uploads/menu/image/".  $items[16]  , 
-        		"subtitle"=>  $items[14]."kcal ".  $items[15]  ."円    ". $items[0]     ,
+        		"subtitle"=>  $items[14]." ".  $items[15]  ."円    ". $items[0]     ,
         		"buttons"=> [
         			[
         			"type"=> "web_url",
@@ -188,11 +188,6 @@ if(  count(  $items  ) < 16 ){
 	
 	
                       $client->request('POST', $path, ['json' => $json]);
-	
-	
-	
-	
-	
 	
 	
 	
